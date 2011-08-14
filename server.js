28bb1787e549dd091f66ff88339e4b362ff27404
Sam Stewart <sam@samstewartapps.com>
2011-08-13 23:17:48 -0700

    Added twitter library

--- server.js
+++ server.js
@@ -52,6 +52,61 @@ function sendNewTweetToWall(tweetObject) {
     wallsocket.emit("new tweet", tweet);
 }
 
+var twitterPoll = new TwitterPoll("hapihack", function(tweet) {
+	console.log("callback: "+tweet.text);
+});
+//twitter polling service
+function TwitterPoll(hashtag, callback) {
+	this.hashtag = hashtag;
+	this.last_id = 0;
+	this.callback = callback;
+	this.poll_interval = 2000;
+	
+	var _this = this;
+	this.poll = function() {
+		console.log("polling..");
+		rest.get("http://search.twitter.com/search.json", {
+			query:{
+				q:"#"+this.hashtag,
+				rpp:20,
+				since_id:this.last_id
+			}
+		}).on('complete', function(data) {
+			//grab tweets
+			var results = data.results;
+			console.log("Num tweets: "+results.length);
+			
+			for (var i=0; i<results.length;i++) {
+				var result = results[i];
+				//edge case since api always returns 1 tweet
+				if (result.id != _this.last_id) {
+					//inform the callback of each new tweet
+					if(_this.callback != null)
+						_this.callback(result);
+				}
+				
+				if(i == 0)
+					_this.last_id = result.id;
+
+				
+				
+			}
+			
+		});
+	}
+	this.onUpdate = function(tweet_callback) {
+		this.callback = tweet_callback;
+	}
+	//set callback
+	this.onUpdate(callback);
+	
+	//start polling (with correct this value)
+	var _this = this;
+	this.interval_id = setInterval(function(t) {
+		_this.poll();
+	}, 2000);
+}
+
 
 
 
